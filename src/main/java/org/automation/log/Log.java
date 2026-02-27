ea44cbc6809d8ffcdbda1b28bdbb67a871ccd616
Tushar <savanttushar97@gmail.com>
2026-02-27 15:24:55 +0530

    change in config file location and some log related

--- src/main/java/org/automation/log/Log.java
+++ src/main/java/org/automation/log/Log.java
@@ -11,13 +11,6 @@ public class Log { ;
 
         Log.info(message);
     }
-//    public static void warn(String message) {
-//        Log.warn(message); }
-//    public static void error(String message) {
-//        Log.error(message); }
-//    public static void fatal(String message) {
-//        Log.fatal(message); }
-//    public static void debug(String message) {
-//        Log.debug(message); }
+
 
 }
