10cd884961b68bc1450aa4637231093755a58ef0
Shinde <2464377@cognizant.com>
2026-03-03 17:38:03 +0530

    Added wait in TC_12

--- src/test/java/testcases/TC_12_Brochure_Download_Check.java
+++ src/test/java/testcases/TC_12_Brochure_Download_Check.java
@@ -13,7 +13,7 @@ public class TC_12_Brochure_Download_Check extends BaseTest {
 
         BrochureDownloadCheck page = new BrochureDownloadCheck(driver,wait);
         boolean ok = page.downloadBrochure();
-        Assert.assertTrue(true);
+        Assert.assertTrue(ok, "Download Failed.");
         Log.info("TC_12_Brochure_Download_Check Passed: Brochure Download Successfully");
     }
 }
